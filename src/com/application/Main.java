0c884695e0ce21a1696024bba4c3d0e902b2d22d
Wallace Rocha dos Santos <wrsantos.developer@gmail.com>
2019-06-21 05:45:43 -0300

    Criando classes Board e Piece

--- src/com/application/Main.java
+++ src/com/application/Main.java
@@ -1,5 +1,6 @@
 package com.application;
 
+import com.boardgame.Board;
 import com.boardgame.Position;
 
 public class Main {
@@ -8,5 +9,7 @@ public class Main {
 
         Position position = new Position(3,5);
         System.out.println(position);
+
+        Board board = new Board(8,8);
     }
 }
