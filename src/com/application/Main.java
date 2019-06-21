a6634cc5e144ded2648e7c252a8a59ec6f53d6df
Wallace Rocha dos Santos <wrsantos.developer@gmail.com>
2019-06-21 05:27:15 -0300

    Criando primeira classe Position

--- src/com/application/Main.java
+++ src/com/application/Main.java
@@ -1,9 +1,12 @@
 package com.application;
 
+import com.boardgame.Position;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        Position position = new Position(3,5);
+        System.out.println(position);
     }
 }
